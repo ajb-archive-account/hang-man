fe4b151dec7a0b49ba29fd0733dc7c6cd61b8990
andybyers21 <andy.byers21@gmail.com>
2020-07-19 16:13:51 +0100

    code complete but not looping correctly

--- main.js
+++ main.js
@@ -1,64 +1,58 @@
-let playerName = prompt("What is your name?");
-console.log("Hello" + " " + playerName);
+  //grab the players name
+  let playerName = prompt("What is your name?");
+  alert("Hello" + " " + playerName);
 
-//Pick a random word
-let wordList = [
-  "command",
-  "jail",
-  "compare",
-  "hurried",
-  "expand",
-  "beds",
-  "snails",
-  "extra",
-  "small",
-  "nose",
-  "ubiquitous",
-  "satisfying",
-  "ski"
-];
+  //word options
+  let wordList = [
+    "command",
+    "jail",
+    "compare",
+    "hurried",
+    "expand",
+    "beds",
+    "snails",
+    "extra",
+    "small",
+    "nose",
+    "ubiquitous",
+    "satisfying",
+    "ski",
+  ];
 
-let randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+  //pick a random word from wordList
+  let randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-let answerArray = [];
-for (let i = 0; i < wordList.length; i++) {
-  answerArray[i] = "_";
-}
-
-let remainingLeters = randomWord.length;
-
-// While the word has not been guessed {
-while (remainingLetters > 0) {
-  alert(answerArray.join(" "));
-}
-
-let guess = prompt("pick a letter or press cancel to exit the game.");
-  if (guess === null) {
-    break;
-  } else if (guess.length !== 1) {
-      alert("Only one letter at a time please.");
-  } else // update game status
-
-
-
-
-  // Show the player their progress
-  // Take input from the player
-  // Update answerArray and remainingLetters for every correct guess
-
-/*
-  If the player wants to quit the game {
-    Quit the game
+  //create an array of "_" for the blank answer
+  let answerArray = [];
+  for (let i = 0; i < wordList.length; i++) {
+    answerArray[i] = "_";
   }
-  Else If the guess is not a single letter {
-    Tell the player to pick a single letter
+
+  let remainingLetters = randomWord.length;
+
+  //join letters in the array to create remainingLetters
+  while (remainingLetters > 0) {
+    //show the player their progress
+    alert(answerArray.join(" "));
   }
-  Else {
-    If the guess is in the word {
-      Update the player's progress with the guess
+
+  //player inputs a letter
+  let guess = prompt("pick a letter or press cancel to exit the game.");
+  //if (guess === null) {
+  //exit the game
+  //break;
+  /*} else*/ if (guess.length !== 1) {
+    alert("Only one letter at a time please.");
+  } else {
+    // update game status with the players guess
+    for (let j = 0; j < randomWord.length; j++) {
+      if (randomWord[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
     }
   }
-}
-*/
 
-// Congratulate the player on guessing the word
+  //show the answer array and congratulate player
+  alert(answerArray.join(" "));
+  alert("Well done, the answer was " + randomWord);
