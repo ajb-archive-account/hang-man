208e23f58db06891418c4f76160d4cc04903d034
andybyers21 <andy.byers21@gmail.com>
2020-07-19 16:41:36 +0100

    fully working

--- main.js
+++ main.js
@@ -1,47 +1,45 @@
-  //grab the players name
-  let playerName = prompt("What is your name?");
-  alert("Hello" + " " + playerName);
+//grab the players name
+let playerName = prompt("What is your name?");
+alert("Hello" + " " + playerName);
 
-  //word options
-  let wordList = [
-    "command",
-    "jail",
-    "compare",
-    "hurried",
-    "expand",
-    "beds",
-    "snails",
-    "extra",
-    "small",
-    "nose",
-    "ubiquitous",
-    "satisfying",
-    "ski",
-  ];
+//word options
+let wordList = [
+  "command",
+  "jail",
+  "compare",
+  "hurried",
+  "expand",
+  "beds",
+  "snails",
+  "extra",
+  "small",
+  "nose",
+  "ubiquitous",
+  "satisfying",
+  "ski",
+];
 
-  //pick a random word from wordList
-  let randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+//pick a random word from wordList
+let randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-  //create an array of "_" for the blank answer
-  let answerArray = [];
-  for (let i = 0; i < wordList.length; i++) {
-    answerArray[i] = "_";
-  }
+//create an array of "_" for the blank answer
+let answerArray = [];
+for (let i = 0; i < randomWord.length; i++) {
+  answerArray[i] = "_";
+}
 
-  let remainingLetters = randomWord.length;
+let remainingLetters = randomWord.length;
 
-  //join letters in the array to create remainingLetters
-  while (remainingLetters > 0) {
-    //show the player their progress
-    alert(answerArray.join(" "));
-  }
-
+//join letters in the array to create remainingLetters
+while (remainingLetters > 0) {
+  //show the player their progress
+  alert(answerArray.join(" "));
   //player inputs a letter
   let guess = prompt("pick a letter or press cancel to exit the game.");
-  //if (guess === null) {
-  //exit the game
-  //break;
-  /*} else*/ if (guess.length !== 1) {
+  if (guess === null) {
+    //exit the game
+    break;
+  } else if (guess.length !== 1) {
     alert("Only one letter at a time please.");
   } else {
     // update game status with the players guess
@@ -52,7 +50,8 @@
       }
     }
   }
+}
 
-  //show the answer array and congratulate player
-  alert(answerArray.join(" "));
-  alert("Well done, the answer was " + randomWord);
+//show the answer array and congratulate player
+alert(answerArray.join(" "));
+alert("Well done, the answer was " + randomWord);
